b773d6437b596e9233c40372729bf37896807e17
WIN-QUKGMR21V2O\user <gaisina_albina@list.ru>
2023-06-01 16:13:50 +0400

    Переопределен метод Get у Map. Откорректирован метод Accept, чтобы MailBox корректно заполнялся

--- src/MailService.java
+++ src/MailService.java
@@ -1,23 +1,40 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Consumer;
 
 public class MailService<T> implements Consumer<MessageDesign<T>> {
+    public static class OverridedHashMap<Key, Value> extends HashMap<Key, Value> {
+        @Override
+        public Value get(Object key) {
+            Value statement;
+            statement = super.get(key);
+            try {
+                if (statement == null) {
+                    statement = (Value) new LinkedList <> ();
+                }
+            } catch (ClassCastException e) {
+                throw new RuntimeException(e);
+            }
+            return statement;
+        }
+    }
 
-    private Map<String, List<T>> mailBox = new HashMap<>();
+    private final Map<String, List<T>> mailBox;
+
+    public MailService() {
+        mailBox = new OverridedHashMap<>();
+    }
 
 
     @Override
     public void accept(MessageDesign<T> t) {
-
         List<T> statement = mailBox.get((t.getTo()));
-        if (statement == null) {
-            statement = new LinkedList<>();
-            statement.add(t.getContent());
-            mailBox.put(t.getTo(), statement);
-        } else {
-            statement.add(t.getContent());
-        }
+        statement.add(t.getContent());
+        mailBox.put(t.getTo(), statement);
     }
+
 
     public Map<String, List<T>> getMailBox() {
         return mailBox;
